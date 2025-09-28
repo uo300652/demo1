44abd919f78ecdce371b7b9e3fc9781de49c83a0
uo300652 <matia@192.168.1.195>
2025-09-28 18:04:09 +0200

    Estado inicial

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,14 +1,10 @@
 package demo;
 
-/**
- * Change from luna
- */
 public class Demo {
 	public static void main(String[] args)
 	{
 		System.out.println("linea 1");
-		System.out.println("linea 1");
-		System.out.println("linea 1");
-		/*Change from mars*/
+		System.out.println("linea 2");
+		System.out.println("linea 3");
 	}
 }
