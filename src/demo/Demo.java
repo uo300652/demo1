7ccfc0f63381e762596dccd714a1a3b4eb3ae7b6
uo300652 <matia@192.168.1.195>
2025-09-28 18:05:34 +0200

    Inicial state

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,14 +4,7 @@ public class Demo {
 	public static void main(String[] args)
 	{
 		System.out.println("linea 1");
-<<<<<<< HEAD
-		System.out.println("linea 1");
-		System.out.println("cambio 3 luna");
-		System.out.println("linea 1");
-		/*Change from mars*/
-=======
 		System.out.println("linea 2");
 		System.out.println("linea 3");
->>>>>>> refs/heads/change3
 	}
 }
