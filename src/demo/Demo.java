0c37dda6bca5f0596657ec1e0cdb2727df435f44
uo300652 <matia@192.168.1.195>
2025-09-28 13:52:08 +0200

    Add print statements to the Demo file

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,5 +1,10 @@
 package demo;
 
 public class Demo {
-
+	public static void main(String[] args)
+	{
+		System.out.println("linea 1");
+		System.out.println("linea 1");
+		System.out.println("linea 1");
+	}
 }
