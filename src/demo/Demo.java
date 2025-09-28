0f9c8eb14306e90b8af1fd98480035917bc4afce
uo300652 <matia@192.168.1.195>
2025-09-28 19:16:26 +0200

    Change 1 master

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,18 +3,13 @@ package demo;
 /**Change from luna**/
 /**Change 2 from luna from master**/
 
-/**Change 1 change-rebase**/
-/**Change 2 change-rebase**/
 public class Demo {
 	public static void main(String[] args)
 	{
 		System.out.println("linea 1");
 		System.out.println("change 3 FIXED");
 		System.out.println("linea 3");
-		/*Change from mars*/
-		/*Change 2 from mars from change2*/
 	}
 	/*Change 1 master*/
-	/*Change 2 master*/
 }
 
